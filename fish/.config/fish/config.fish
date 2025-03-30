5eb472d94fb58f2129ac7ce313e34f31090c5d53
leanhquanea1234 <leanhquanm@gmail.com>
2025-03-30 16:07:58 +0700

    add variables

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,4 +1,7 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+set -Ux QT_QPA_PLATFORMTHEME qt6ct
+
 starship init fish | source
+
+alias gs "git status"
+alias tm "tmux"
+alias gca "git commit -a"
