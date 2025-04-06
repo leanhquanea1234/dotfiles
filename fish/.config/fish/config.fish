31c710234356c71ad6ca36ce3930076e47395dfd
leanhquanea1234 <leanhquanm@gmail.com>
2025-04-06 18:25:10 +0700

    Add clang-format

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,7 @@ set -Ux QT_QPA_PLATFORMTHEME qt6ct
 
 starship init fish | source
 
+alias nf "neofetch"
 alias gs "git status"
 alias tm "tmux"
 alias gca "git commit -a"
--- neovim/.config/nvim/lua/plugins/lsp-config.lua
+++ neovim/.config/nvim/lua/plugins/lsp-config.lua
@@ -12,7 +12,7 @@ return {
         config = function()
             -- ensure that we have lua language server, typescript launguage server, java language server, and java test language server are installed
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls",  "jdtls", "clangd"},
+                ensure_installed = { "lua_ls", "jdtls", "clangd" },
             })
         end
     },
@@ -41,16 +41,20 @@ return {
 
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            require'lspconfig'.clangd.setup{}
+            require 'lspconfig'.clangd.setup {
+                capabilities = {
+                    offsetEncoding = { "utf-16" }, -- Fix for "received offset encoding UTF-16"
+                },
+            }
             -- setup the lua language server
             lspconfig.lua_ls.setup({
                 capabilities = capabilities,
             })
 
             -- setup the typescript language server
---            lspconfig.tsserver.setup({
---                capabilities = capabilities,
---            })
+            --            lspconfig.tsserver.setup({
+            --                capabilities = capabilities,
+            --            })
 
             -- Set vim motion for <Space> + c + h to show code documentation about the code the cursor is currently over if available
             vim.keymap.set("n", "<leader>ch", vim.lsp.buf.hover, { desc = "[C]ode [H]over Documentation" })
@@ -59,9 +63,11 @@ return {
             -- Set vim motion for <Space> + c + a for display code action suggestions for code diagnostics in both normal and visual mode
             vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ctions" })
             -- Set vim motion for <Space> + c + r to display references to the code under the cursor
-            vim.keymap.set("n", "<leader>cr", require("telescope.builtin").lsp_references, { desc = "[C]ode Goto [R]eferences" })
+            vim.keymap.set("n", "<leader>cr", require("telescope.builtin").lsp_references,
+                { desc = "[C]ode Goto [R]eferences" })
             -- Set vim motion for <Space> + c + i to display implementations to the code under the cursor
-            vim.keymap.set("n", "<leader>ci", require("telescope.builtin").lsp_implementations, { desc = "[C]ode Goto [I]mplementations" })
+            vim.keymap.set("n", "<leader>ci", require("telescope.builtin").lsp_implementations,
+                { desc = "[C]ode Goto [I]mplementations" })
             -- Set a vim motion for <Space> + c + <Shift>R to smartly rename the code under the cursor
             vim.keymap.set("n", "<leader>cR", vim.lsp.buf.rename, { desc = "[C]ode [R]ename" })
             -- Set a vim motion for <Space> + c + <Shift>D to go to where the code/object was declared in the project (class file)
