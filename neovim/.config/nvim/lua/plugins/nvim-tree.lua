c90728b53e316fce0239c28211e17ddf7f806dca
leanhquanea1234 <leanhquanm@gmail.com>
2025-04-21 08:14:07 +0700

    show file even in gitignore

--- neovim/.config/nvim/lua/plugins/nvim-tree.lua
+++ neovim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -1,14 +1,21 @@
 return {
-    "nvim-tree/nvim-tree.lua",
-    config = function()
-        vim.keymap.set('n', '<C-n>', "<cmd>NvimTreeToggle<CR>", { desc = "Toggle [E]xplorer" })
-        require("nvim-tree").setup({
-            hijack_netrw = true,
-            auto_reload_on_write = true,
-            view = {
-                side = "right", -- Positions the tree on the right side
-                width = 30,
-            },
-        })
-    end
+  "nvim-tree/nvim-tree.lua",
+  config = function()
+    vim.keymap.set('n', '<C-n>', "<cmd>NvimTreeToggle<CR>", { desc = "Toggle [E]xplorer" })
+    require("nvim-tree").setup({
+      hijack_netrw = true,
+      auto_reload_on_write = true,
+      view = {
+        side = "right",         -- Positions the tree on the right side
+        width = 30,
+      },
+      git = {
+        enable = true, -- Enable Git integration
+        ignore = false, -- Do NOT hide files in .gitignore
+      },
+      filters = {
+        dotfiles = false, -- Optional: Show dotfiles (e.g., .config)
+      },
+    })
+  end
 }
